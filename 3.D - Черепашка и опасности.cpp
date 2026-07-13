fc51d3a0a2d640396117894c3222e58d4b5c6b19
andrey kochev m8o - 108bv <mobzila20.19@gmail.com>
2026-07-13 10:13:34 +0300

    Update 3.D - Черепашка и опасности.cpp

--- 3.D - Черепашка и опасности.cpp
+++ 3.D - Черепашка и опасности.cpp
@@ -1,30 +1,30 @@
 #include <iostream>
 #include <vector>
- 
 using namespace std;
- 
+
 const int MOD = 1000000007;
- 
+
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
     
     int n, m, k;
     cin >> n >> m >> k;
     
-    vector<vector<bool>> dangerous(n + 1, vector<bool>(m + 1, false));
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+    vector<vector<bool>> danger(n + 1, vector<bool>(m + 1, false));
+    
     for (int i = 0; i < k; i++) {
         int x, y;
         cin >> x >> y;
-        dangerous[x][y] = true;
+        danger[x][y] = true;
     }
     
-    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
     dp[0][0] = 1;
     
     for (int i = 0; i <= n; i++) {
         for (int j = 0; j <= m; j++) {
-            if (dangerous[i][j]) {
+            if (danger[i][j]) {
                 dp[i][j] = 0;
                 continue;
             }
@@ -37,7 +37,7 @@ int main() {
         }
     }
     
-    cout << dp[n][m] << "\n";
+    cout << dp[n][m] % MOD << endl;
     
     return 0;
 }
