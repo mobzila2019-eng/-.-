b41ef3f7d88d26165dfcf6f82267832257eab40b
andrey kochev m8o - 108bv <mobzila20.19@gmail.com>
2026-07-13 10:21:05 +0300

    Update 7.A - Алгоритм Форда-Беллмана.cpp

--- 7.A - Алгоритм Форда-Беллмана.cpp
+++ 7.A - Алгоритм Форда-Беллмана.cpp
@@ -1,72 +1,60 @@
 #include <iostream>
 #include <vector>
-#include <limits>
- 
 using namespace std;
- 
-const long long INF = 1e18;
- 
+
+struct Edge {
+    int u, v;
+    long long w;
+};
+
+const long long INF = 4e18;
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
- 
+    
     int n, m, s;
     cin >> n >> m >> s;
-    s--;
- 
-    vector<vector<pair<int, int>>> graph(n);
-    for (int i = 0; i < m; ++i) {
-        int u, v, w;
-        cin >> u >> v >> w;
-        u--; v--;
-        graph[u].emplace_back(v, w);
+    
+    vector<Edge> edges(m);
+    for (int i = 0; i < m; i++) {
+        cin >> edges[i].u >> edges[i].v >> edges[i].w;
     }
- 
-    vector<long long> dist(n, INF);
+    
+    vector<long long> dist(n + 1, INF);
     dist[s] = 0;
- 
-    for (int i = 0; i < n - 1; ++i) {
-        bool updated = false;
-        for (int u = 0; u < n; ++u) {
-            if (dist[u] == INF) continue;
-            for (auto& edge : graph[u]) {
-                int v = edge.first;
-                int w = edge.second;
-                if (dist[u] + w < dist[v]) {
-                    dist[v] = dist[u] + w;
-                    updated = true;
-                }
+    
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < m; j++) {
+            int u = edges[j].u;
+            int v = edges[j].v;
+            long long w = edges[j].w;
+            
+            if (dist[u] != INF && dist[u] + w < dist[v]) {
+                dist[v] = dist[u] + w;
             }
         }
-        if (!updated) break;
     }
- 
-    bool has_negative_cycle = false;
-    for (int u = 0; u < n; ++u) {
-        if (dist[u] == INF) continue;
-        for (auto& edge : graph[u]) {
-            int v = edge.first;
-            int w = edge.second;
-            if (dist[u] + w < dist[v]) {
-                has_negative_cycle = true;
-                break;
-            }
+    
+    for (int j = 0; j < m; j++) {
+        int u = edges[j].u;
+        int v = edges[j].v;
+        long long w = edges[j].w;
+        
+        if (dist[u] != INF && dist[u] + w < dist[v]) {
+            cout << "Negative cycle" << endl;
+            return 0;
         }
-        if (has_negative_cycle) break;
     }
- 
-    if (has_negative_cycle) {
-        cout << "Negative cycle\n";
-    } else {
-        for (int i = 0; i < n; ++i) {
-            if (dist[i] == INF) {
-                cout << "inf ";
-            } else {
-                cout << dist[i] << " ";
-            }
+    
+    for (int i = 1; i <= n; i++) {
+        if (dist[i] == INF) {
+            cout << "inf ";
+        } else {
+            cout << dist[i] << " ";
         }
-        cout << "\n";
     }
- 
+    cout << endl;
+    
     return 0;
 }
